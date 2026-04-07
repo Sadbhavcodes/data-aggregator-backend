521f15342e3a36f9bff57424a8eb446ac46e39f2
user1 <24ucs036@lnmiit.ac.in>
2026-04-07 20:10:32 +0530

    Day 5

--- src/controllers/authController.ts
+++ src/controllers/authController.ts
@@ -1,21 +1,70 @@
-import { userModel } from "../models/User";
 import {Request, Response} from 'express';
-import {SignupService} from '../services/authServices';
+import {SignupService, loginService} from '../services/authServices';
 
 export const signup = async(req: Request, res: Response) => {
     try{
         const {username, email, password} = req.body;
         const result = await SignupService(username, email, password);
-
+        if(result.success){
+            res.cookie('token', result.token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'strict',
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            })
+            return res.status(200).json({
+                success: true,
+                message: result.message,
+                user: result.user
+            })
+        }
+        return res.status(400).json({
+            message: result.message
+        })
     }catch(error){
-
+        return res.status(500).json({
+            message: "Internal server error"
+        })
     }
 }
 
 export const login = async (req: Request, res:Response) => {
     try{
         const {email, password} = req.body;
+        const result = await loginService(email, password);
+        if(result.success){
+            res.cookie('token', result.token, {
+                httpOnly: true,
+                secure: true,
+                sameSite: 'strict',
+                maxAge: 7 * 24 * 60 * 60 * 1000
+            })
+            return res.status(200).json({
+                success: true,
+                message: result.message,
+                user: result.user
+            })
+        }
+        return res.status(400).json({
+            message: result.message
+        })
     }catch(error){
+        return res.status(500).json({
+            message: "Internal server error"
+        })
+    }
+}
 
+export const logout = async (req: Request, res: Response) => {
+    try{
+        res.clearCookie('token');
+        return res.status(200).json({
+            success: true,
+            message: "Logged out successfully"
+        })
+    }catch(error){
+        return res.status(500).json({
+            message: "Internal server error"
+        })
     }
 }
--- src/services/authServices.ts
+++ src/services/authServices.ts
@@ -22,7 +22,7 @@ export const SignupService = async (
                 message: 'user already exists'
             }
         }
-        const hashedPass = await bcrypt.hash(password, 10);
+        const hashedPass = await bcrypt.hash(password, 12);
 
         const user = await userModel.create({
             username,
@@ -46,6 +46,47 @@ export const SignupService = async (
         }
     }catch(error){
         console.error("Signup Service Error:", error);
+        return {
+            success: false,
+            message: "Internal server error",
+        }
+    }
+}
+
+export const loginService = async (
+    email: string,
+    password: string
+) => {
+    try{
+        const userExist = await userModel.findOne({email}).select('+password');
+        if(userExist){
+            const isMatch = await bcrypt.compare(password, userExist.password as string);
+            if(isMatch){
+                const id = userExist._id.toString();
+                const token = generateToken(id ,userExist.role);
+                return {
+                    success: true,
+                    message: 'user logged in successfully',
+                    token,
+                    user: {
+                        id,
+                        username: userExist.username,
+                        email: userExist.email,
+                        role: userExist.role
+                    }
+                }
+            }
+            return {
+                success: false,
+                message: 'invalid credentials'
+            }
+        }   
+        return {
+            success: false,
+            message: 'user not found'
+        }
+    }catch(error){
+        console.error("Login Service Error:", error);
         return {
             success: false,
             message: "Internal server error",
